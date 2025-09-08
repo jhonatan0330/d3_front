798e55c9cf23de396465e410ad125cee319e2b11
Jhonatan garcia <jhonatan.garcia@colombiansofture.com>
2025-09-08 13:05:11 -0500

    v16

--- src/app/authentication/authentication.guard.ts
+++ src/app/authentication/authentication.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild,  Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, of, switchMap } from 'rxjs';
 import { LoginService } from './login.service';
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanActivateChild {
+export class AuthGuard  {
     /**
      * Constructor
      */
