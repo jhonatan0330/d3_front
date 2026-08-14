63864b124f008190642c4ec7ac23e0a0f70032f5
jhonatan0330 <jhonatan0330@hotmail.com>
2026-08-14 10:34:21 -0500

    style(recover-password): unify with sign-in style - canvas, tailwind inputs, dark mode

--- src/app/authentication/recover-password/recover-password.component.ts
+++ src/app/authentication/recover-password/recover-password.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, inject, viewChild } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { Validators, FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Subject } from 'rxjs';
@@ -7,25 +6,24 @@ import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import Swal from 'sweetalert2';
 import { LoginService } from '../login.service';
 import { Router, RouterLink } from '@angular/router';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
+import { ParticleBackgroundDirective } from '../shared/particle-background';
 
 @Component({
     selector: 'app-recover-password',
     providers: [Location, { provide: LocationStrategy, useClass: PathLocationStrategy }],
     templateUrl: './recover-password.component.html',
     changeDetection: ChangeDetectionStrategy.Eager,
-    imports: [FormsModule, ReactiveFormsModule, MatFormField, MatLabel, MatInput, MatButton, RouterLink, MatProgressBar]
+    imports: [FormsModule, ReactiveFormsModule, RouterLink, MatProgressBar, ParticleBackgroundDirective]
 })
 export class RecoverPasswordComponent implements OnInit, OnDestroy {
   private loginService = inject(LoginService);
   private router = inject(Router);
 
   readonly progressBar = viewChild(MatProgressBar);
-  readonly submitButton = viewChild(MatButton);
 
   recoverForm: FormGroup<{ identificacion: FormControl<string | null>, correo: FormControl<string | null> }>;
   errorMsg = '';
+  submitting = false;
   
   private _unsubscribeAll: Subject<any>;
 
@@ -49,7 +47,7 @@ export class RecoverPasswordComponent implements OnInit, OnDestroy {
   signin() {
     const signinData = this.recoverForm.value;
 
-    this.submitButton()!.disabled = true;
+    this.submitting = true;
     this.progressBar()!.mode = 'indeterminate';
 
     this.loginService.recoverPassword(signinData.identificacion!, signinData.correo!).subscribe({
@@ -59,7 +57,7 @@ export class RecoverPasswordComponent implements OnInit, OnDestroy {
         this.router.navigateByUrl('main');
       },
       error: () => {
-        this.submitButton()!.disabled = false;
+        this.submitting = false;
         this.progressBar()!.mode = 'determinate';
       }
     });
