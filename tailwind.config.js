f894470deedd633504b3d0f9910b342ef427ed82
jhonatan0330 <jhonatan0330@hotmail.com>
2026-09-18 19:43:07 -0500

    perf(styles): reduce Tailwind content scan

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,7 +65,7 @@ const themes = {
  */
 const config = {
     darkMode   : 'class',
-    content    : ['./src/**/*.{html,scss,ts}'],
+    content    : ['./src/**/*.{html,ts}'],
     important  : true,
     theme      : {
         fontSize: {
